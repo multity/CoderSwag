302f68ba043c5d9c20cb2bce2f50d87bb42df7b9
Dimitar Todorov <d.todorov.vn@gmail.com>
2018-09-27 23:41:32 +0300

    Lesson-03 - Add Custom list adapter and View holder

--- app/src/main/java/com/example/dtodorov/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/dtodorov/coderswag/Controller/MainActivity.kt
@@ -2,19 +2,18 @@ package com.example.dtodorov.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import com.example.dtodorov.coderswag.Model.Category
+import com.example.dtodorov.coderswag.Adapters.CategoryAdapter
 import com.example.dtodorov.coderswag.R
 import com.example.dtodorov.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.categories)
+        adapter = CategoryAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
     }
 }
