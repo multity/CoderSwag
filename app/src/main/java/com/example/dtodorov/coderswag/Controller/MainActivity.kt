28d0a1216cfbef876ea3bfcbe1550a6385857eb0
Dimitar Todorov <d.todorov.vn@gmail.com>
2018-09-30 20:44:11 +0300

    Lesson-04 - Add Categories recycle view Adapter

--- app/src/main/java/com/example/dtodorov/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/dtodorov/coderswag/Controller/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.dtodorov.coderswag.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import com.example.dtodorov.coderswag.Adapters.CategoryAdapter
 import com.example.dtodorov.coderswag.Adapters.CategoryRecycleAdapter
+import com.example.dtodorov.coderswag.ProductsActivity
 import com.example.dtodorov.coderswag.R
 import com.example.dtodorov.coderswag.Services.DataService
+import com.example.dtodorov.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +19,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this,DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories) {category ->
+            val productIntent = Intent(this,ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
+        }
         categoryListView.adapter = adapter
 
         val layoutManager = LinearLayoutManager(this)
